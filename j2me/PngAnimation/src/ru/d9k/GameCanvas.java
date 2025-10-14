870cf5f10e3a31bd97a370d98f829baf6f334503
d9k <d9k@ya.ru>
2025-10-15 04:13:36 +0500

    [draft] j2me PngAnimation: refactor: extract GameCanvas

--- j2me/PngAnimation/src/ru/d9k/GameCanvas.java
+++ j2me/PngAnimation/src/ru/d9k/GameCanvas.java
@@ -1,52 +1,9 @@
+package ru.d9k;
+
+import java.io.IOException;
 import javax.microedition.lcdui.Canvas;
-import javax.microedition.lcdui.Display;
 import javax.microedition.lcdui.Graphics;
 import javax.microedition.lcdui.Image;
-import javax.microedition.midlet.MIDlet;
-import java.io.IOException;
-
-/*
- * This is the main class of the hello world demo.
- */
-public class PngAnimationMIDlet extends MIDlet {
-
-	private GameCanvas gameCanvas;
-
-	/**
-	 * This is constructor of class HelloWorld. The Command objects appear as
-	 * buttons.
-	 */
-	public PngAnimationMIDlet() {
-
-	}
-	//  implementation of MIDlet
-
-	/**
-	 * Start the application.
-	 */
-	protected void startApp() {
-		try {
-			gameCanvas = new GameCanvas();
-			Display.getDisplay(this).setCurrent(gameCanvas);
-			new Thread(gameCanvas).start();
-		} catch (IOException e) {
-
-		}
-	}
-
-	/**
-	 * This method is called to notify the MIDlet to enter a paused state. The
-	 * MIDlet should use this opportunity to release shared resources.
-	 */
-	protected void pauseApp() {
-	}
-
-	/**
-	 * If the MIDlet was using resources, it should release them in this method.
-	 */
-	protected void destroyApp(boolean bool) {
-	}
-}
 
 class GameCanvas extends Canvas implements Runnable {
 
