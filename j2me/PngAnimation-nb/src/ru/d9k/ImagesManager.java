3ae1a4958b078fbdf92f28d23e610d20ecd6dbd0
d9k <d9k@ya.ru>
2026-01-08 10:30:56 +0500

    j2me PngAnimation (NetBeans): Kemulator runtime error note

--- j2me/PngAnimation-nb/src/ru/d9k/ImagesManager.java
+++ j2me/PngAnimation-nb/src/ru/d9k/ImagesManager.java
@@ -5,16 +5,25 @@ import javax.microedition.lcdui.Image;
 public class ImagesManager {
    public Image[] getPigRightArray() throws IOException {
 		 int len = 4;
+//		 int len = 2;
 		 Image[] result = new Image[len];
 
 		 for (int ix = 0; ix < len; ix++) {
 			 int fileNumber = ix + 1;
 			 System.out.println("ix=" + ix +", fileNumber=" + fileNumber);
-		   result[ix] = Image.createImage("/characters/pig-right-" + fileNumber + ".png");
+
+			 /**
+				* TODO Without Thread.sleep() there is runtime error on Kemulator
+				* (https://github.com/shinovon/KEmulator/issues/270)
+				* when built with NetBeans project.
+				* (No error with IDEA + Kemulator integration)
+				*/
+
 //				try {
 //					Thread.sleep(10);
 //				} catch (InterruptedException e) {
 //				}
+		   result[ix] = Image.createImage("/characters/pig-right-" + fileNumber + ".png");
 //			 result[ix] = Image.createImage("/characters/pig-right-1.png");
 		 }
 
