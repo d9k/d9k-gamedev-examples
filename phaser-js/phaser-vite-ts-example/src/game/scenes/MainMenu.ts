271b54c106bdff23ab6bfaef0ace3ba6b5b5382e
d9k <d9k@ya.ru>
2026-04-10 16:31:52 +0500

    phaser-vite-ts-example: show game title from const

--- phaser-js/phaser-vite-ts-example/src/game/scenes/MainMenu.ts
+++ phaser-js/phaser-vite-ts-example/src/game/scenes/MainMenu.ts
@@ -1,10 +1,12 @@
 import { Scene, GameObjects } from 'phaser';
+import { GAME_TITLE } from '~/const/main';
 
 export class MainMenu extends Scene
 {
     background: GameObjects.Image;
     logo: GameObjects.Image;
-    title: GameObjects.Text;
+    sceneTitle: GameObjects.Text;
+    gameTitle: GameObjects.Text;
 
     constructor ()
     {
@@ -17,11 +19,17 @@ export class MainMenu extends Scene
 
         this.logo = this.add.image(512, 300, 'logo');
 
-        this.title = this.add.text(512, 460, 'Main Menu', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
+        const textStyle =  {
+            fontFamily: 'Arial Black',
+            fontSize: 38,
+            color: '#ffffff',
+            stroke: '#000000',
+            strokeThickness: 8,
             align: 'center'
-        }).setOrigin(0.5);
+        }
+
+        this.gameTitle = this.add.text(512, 420, GAME_TITLE, {...textStyle, color: '#ffff00', stroke: '#222222'}).setOrigin(0.5);
+        this.sceneTitle = this.add.text(512, 500, 'Main Menu', textStyle).setOrigin(0.5);
 
         this.input.once('pointerdown', () => {
 
